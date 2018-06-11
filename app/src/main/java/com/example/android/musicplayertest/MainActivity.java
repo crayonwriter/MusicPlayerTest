b133ef5cf720c8e913ad058b3a7602f969f9019a
Theda Rogers <crayonwriter@gmail.com>
2018-06-11 01:28:34 -0400

    Connected play and pause buttons

--- app/src/main/java/com/example/android/musicplayertest/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayertest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicplayertest;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.winter_wren);
+
+
+        final Button playButton = (findViewById(R.id.playButton));
+        final Button pauseButton = (findViewById(R.id.pauseButton));
+        //create an onclicklistener for each button
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+
     }
 }
